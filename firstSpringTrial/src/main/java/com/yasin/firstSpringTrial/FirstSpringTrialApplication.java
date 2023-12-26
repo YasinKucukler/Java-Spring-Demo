d21bfd6f1fe01b607aee0bcd7c8a184914c20d06
YasinKucukler <yasinkucukler48@gmail.com>
2023-12-27 00:56:01 +0300

    first demo step 6

--- firstSpringTrial/src/main/java/com/yasin/firstSpringTrial/FirstSpringTrialApplication.java
+++ firstSpringTrial/src/main/java/com/yasin/firstSpringTrial/FirstSpringTrialApplication.java
@@ -1,9 +1,7 @@
 package com.yasin.firstSpringTrial;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FirstSpringTrialApplication {
