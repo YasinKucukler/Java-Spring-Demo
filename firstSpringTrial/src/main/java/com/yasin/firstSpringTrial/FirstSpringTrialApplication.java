fe34970e689f6e50e8fdf8bee8ca308d5f4b8515
YasinKucukler <yasinkucukler48@gmail.com>
2023-12-26 18:39:31 +0300

    first demo step 5

---
+++ firstSpringTrial/src/main/java/com/yasin/firstSpringTrial/FirstSpringTrialApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.firstSpringTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringTrialApplication.class, args);
+	}
+
+}
---
+++ firstSpringTrial/src/test/java/com/yasin/firstSpringTrial/FirstSpringTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasin.firstSpringTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
